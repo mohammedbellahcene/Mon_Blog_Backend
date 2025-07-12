0cb3fcbc8af4e25d4f766a45f3e7266a124dc89c
m%ohammed Bellahcene <mohammedbellahcene95@gmail.com>
2025-07-12 03:56:18 +0200

    users posts comments themes s'affiche sur le dashbord

--- src/main/java/com/blog/api/controller/AdminCategoryThemeController.java
+++ src/main/java/com/blog/api/controller/AdminCategoryThemeController.java
@@ -9,7 +9,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/admin")
+@RequestMapping("/admin")
 @RequiredArgsConstructor
 @PreAuthorize("hasRole('ADMIN')")
 public class AdminCategoryThemeController {
